4190da90bf9d2b2d890f3716f397085cb0b9739d
nelsonic <nodecoder@gmail.com>
2014-06-06 22:02:18 +0100

    getting Error: Invalid routeConfig options (/users/{username}) {

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ This code is for **Testing Hapi Services with Lab** by
 [Fionn Kelleher](https://twitter.com/_expr)
 https://medium.com/the-spumko-suite/testing-hapi-services-with-lab-96ac463c490a
 
+followed the tutorial but getting:
+
+> Error: Invalid routeConfig options (/users/{username}) {
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require("hapi");
+var Joi = require("joi");
 var server = new Hapi.Server(8080);
 var database = require("./database.json");
 
@@ -7,6 +8,65 @@ server.route({
     method: "GET",
     handler: function(request, reply) {
 		reply(Object.keys(database));
+    }
+});
+
+server.route({
+    path: "/users/{username}",
+    method: "GET",
+    config: {
+        validate: {
+            path: { username: Joi.string().token() }
+        },
+        handler: function(request, reply) {
+            if (database[request.params.username]) {
+                reply(database[request.params.username]);
+            } else {
+                reply(Hapi.error.notFound("User not found."));
+            }
+        }
+    }
+});
+ 
+server.route({
+    path: "/users/{username}",
+    method: "PUT",
+    config: {
+        validate: {
+            path: { username: Joi.string().token() },
+            payload: {
+                full_name: Joi.string(),
+                age: Joi.number().integer(),
+                image: Joi.string()
+            }
+        },
+        handler: function(request, reply) {
+            if (!database[request.params.username]) {
+                database[request.params.username] = request.payload;
+                database[request.params.username].count = 0;
+                reply(database[request.params.username]);
+            } else {
+                reply(Hapi.error.conflict("User already exists."));
+            }
+        }
+    }
+});
+ 
+server.route({
+    path: "/users/{username}",
+    method: "DELETE",
+    config: {
+        validate: {
+            path: { username: Joi.string().token() }
+        },
+        handler: function(request, reply) {
+            if (!database[request.params.username]) {
+                reply(Hapi.error.notFound("User not found."))
+            } else {
+                delete database[request.params.username];
+                reply({result: "success"});
+            }
+        }
     }
 });
 
