a99f894516a4eddf1453c3fedd5b8969444ac35a
nelsonic <nodecoder@gmail.com>
2014-06-06 21:57:41 +0100

    first test passes

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 hapi-testing-with-lob
 =====================
 
-Repo to demo hapi testing
+This code is for **Testing Hapi Services with Lab** by 
+[Fionn Kelleher](https://twitter.com/_expr)
+https://medium.com/the-spumko-suite/testing-hapi-services-with-lab-96ac463c490a
+
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var Hapi = require("hapi");
 var server = new Hapi.Server(8080);
+var database = require("./database.json");
+
+server.route({
+    path: "/users",
+    method: "GET",
+    handler: function(request, reply) {
+		reply(Object.keys(database));
+    }
+});
+
 if (!module.parent) {
     server.start(function() {
         console.log("Server started", server.info.uri);
